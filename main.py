6bdd0df67e2248dbad1b88eb61d857768d9c5cbb
Federico Zenteno <a01660986@tec.mx>
2024-10-13 10:49:16 -0600

    final commit

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from api.endpoints.quiz import router as quiz_router
 from api.endpoints.user_endpoint import router as user_router
 from api.endpoints.context_evaluation import router as context_evaluation_router
-
+from api.endpoints.chatbot import router as chatbot_router
 app = FastAPI()
 
 origins = [
@@ -23,7 +23,7 @@ app.add_middleware(
 app.include_router(quiz_router, prefix="/api/quiz")
 app.include_router(user_router, prefix="/api/user")
 app.include_router(context_evaluation_router, prefix="/api/user")
-
+app.include_router(chatbot_router, prefix="/api/user")
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
